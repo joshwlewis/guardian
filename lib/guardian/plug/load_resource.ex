8ce33d6333c1314ed9fbe52717cd8dddf80bffb2
Josh W Lewis <josh.w.lewis@gmail.com>
2016-03-22 23:13:30 -0500

    Add error handling for LoadResource

--- lib/guardian/plug/load_resource.ex
+++ lib/guardian/plug/load_resource.ex
@@ -10,11 +10,24 @@ defmodule Guardian.Plug.LoadResource do
 
   If there is no valid JWT in the request so far (Guardian.Plug.VerifySession /
   Guardian.Plug.VerifyHeader) did not find a valid token
-  then nothing will occur, and the Guardian.Plug.current_resource/1 will be nil
+  then nothing will occur, and the Guardian.Plug.current_resource/1 will be
+  nil.
+
+  A module may be specified as the `:handler` option to catch any errors
+  arising from resource deserialization. The specified  module will need to
+  implement `invalid_resource/2`. If no handler is specified,
+  `Guardian.Plug.current_resource/1` will be nil on any deserialization error.
   """
+  import Plug.Conn
 
   @doc false
-  def init(opts \\ %{}), do: Enum.into(opts, %{})
+  def init(opts \\ %{}) do
+    Enum.into(opts, %{})
+    case opts.handler do
+      nil -> opts
+      mod -> Enum.put(:handler, {mod, :invalid_resource})
+    end
+  end
 
   @doc false
   def call(conn, opts) do
@@ -27,18 +40,29 @@ defmodule Guardian.Plug.LoadResource do
         case Guardian.Plug.claims(conn, key) do
           {:ok, claims} ->
             result = Guardian.serializer.from_token(Map.get(claims, "sub"))
-            set_current_resource_from_serializer(conn, key, result)
-          {:error, _} -> Guardian.Plug.set_current_resource(conn, nil, key)
-          _ -> Guardian.Plug.set_current_resource(conn, nil, key)
+            set_current_resource_from_serializer(conn, key, result, opts)
+          _ ->
+            Guardian.Plug.set_current_resource(conn, nil, key)
         end
     end
   end
 
-  defp set_current_resource_from_serializer(conn, key, {:ok, resource}) do
+  defp set_current_resource_from_serializer(conn, key, {:ok, resource}, _opts) do
     Guardian.Plug.set_current_resource(conn, resource, key)
   end
 
-  defp set_current_resource_from_serializer(conn, key, {:error, _}) do
+  defp set_current_resource_from_serializer(conn, key, reason, opts) do
     Guardian.Plug.set_current_resource(conn, nil, key)
+    |> handle_error(reason, opts)
+  end
+
+  def handle_error(conn, reason, opts) do
+    case Map.get(opts, :handler) do
+      {mod, meth} ->
+        connection = conn |> assign(:guardian_failure, reason) |> halt
+        options = Map.merge(connection.params, %{reason: reason})
+        apply(mod, meth, [connection, options])
+      _ -> conn
+    end
   end
 end
